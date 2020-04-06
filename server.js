f78d7b2ee853d9a2d857da910bfa789a991f8a54
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-04-06 16:34:32 +0300

    Add A Basic Routes

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get('/api/v1/boocamps', (req, res) => {
+    res.status(200).json({success: true, msg: "Show all bootcamps", data: [], error: []});
+})
+
+
+app.put('/api/v1/boocamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Show bootcamp ${req.params.id}`, data: [], error: []});
+})
+
+app.post('/api/v1/boocamps', (req, res) => {
+    res.status(200).json({success: true, msg: "Create new bootcamp", data: [], error: []});
+})
+
+app.put('/api/v1/boocamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`, data: [], error: []});
+})
+
+app.delete('/api/v1/boocamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`, data: [], error: []});
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
