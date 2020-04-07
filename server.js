57d934e863bd9fd0de5cc7a46742592f1df53489
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-04-07 22:22:45 +0300

    Create Connection to DB

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-// Route Files
-const bootcampRoutes = require('./routes/bootcamps');
-
-// Middleware files
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
+// Route Files
+const bootcampRoutes = require('./routes/bootcamps');
 
 const app = express();
 
@@ -19,6 +20,14 @@ app.use('/api/v1/bootcamps', bootcampRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections.
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & Exit process
+  server.close(() => process.exit(1));
+})
