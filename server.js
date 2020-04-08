5291311c3441ccc5ad0b6cf3be166cec124a11f5
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-04-08 10:47:28 +0300

    Create Bootcamp Schema

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -21,13 +22,13 @@ app.use('/api/v1/bootcamps', bootcampRoutes);
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgYellow.blue.bold)
 );
 
 // Handle unhandled promise rejections.
 
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
+  console.log(`Error: ${err.message}`.red);
   // Close server & Exit process
   server.close(() => process.exit(1));
 })
