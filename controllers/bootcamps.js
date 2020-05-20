82f7360c850fd966a9b1c7ba813869f75dfc69f8
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-05-21 02:41:01 +0300

    make Bootcamp User Relationship

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,7 +2,7 @@ const path = require("path");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const geocoder = require("../utils/geocoder");
-const BootCamp = require("../models/Bootcamp");
+const Bootcamp = require("../models/Bootcamp");
 
 // @desc        get all bootcamps
 // @route       GET /api/v1/bootcamps
@@ -29,7 +29,7 @@ const getBootcamps = asyncHandler(async (req, res, next) => {
   // );
 
   // // Finding resource
-  // query = BootCamp.find(JSON.parse(queryStr)).populate("courses");
+  // query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
 
   // if (req.query.select) {
   //   const fields = req.query.select.split(",").join(" ");
@@ -48,7 +48,7 @@ const getBootcamps = asyncHandler(async (req, res, next) => {
   // const limit = parseInt(req.query.limit, 10) || 10;
   // const startIndex = (page - 1) * limit;
   // const endIndex = page * limit;
-  // const total = await BootCamp.countDocuments();
+  // const total = await Bootcamp.countDocuments();
 
   // query = query.skip(startIndex).limit(limit);
 
@@ -79,7 +79,7 @@ const getBootcamps = asyncHandler(async (req, res, next) => {
 // @route       GET /api/v1/bootcamps
 // @access      public
 const getBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await BootCamp.findById(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
@@ -96,6 +96,17 @@ const getBootcamp = asyncHandler(async (req, res, next) => {
 // @route       POST /api/v1/bootcamps
 // @access      private
 const createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add use to req.body
+  req.body.user = req.user.id;
+
+  // Checked of published bootcamps
+  const publishedBootcamps = await Bootcamp.findById({ user: req.user.id });
+
+  // If the user is not admin/ they can only add one Bootcamp
+  if(publishedBootcamps && req.user.role !== "Admin") {
+    return next(new ErrorResponse(`The User With ID ${req.user.id} has already published a bootcamp`, 400));
+  }
+
   const bootcamp = await BootCamp.create(req.body);
   res.status(201).json({
     success: true,
@@ -108,7 +119,7 @@ const createBootcamp = asyncHandler(async (req, res, next) => {
 // @route       PUT /api/v1/bootcamps/:id
 // @access      private
 const updateBootcamps = asyncHandler(async (req, res, next) => {
-  const updatedBootcamp = await BootCamp.findByIdAndUpdate(
+  const updatedBootcamp = await Bootcamp.findByIdAndUpdate(
     req.params.id,
     req.body,
     {
@@ -132,7 +143,7 @@ const updateBootcamps = asyncHandler(async (req, res, next) => {
 // @route       DELETE /api/v1/bootcamps/:id
 // @access      private
 const deleteBootcamps = asyncHandler(async (req, res, next) => {
-  const deletedBootcamp = await BootCamp.findById(req.params.id);
+  const deletedBootcamp = await Bootcamp.findById(req.params.id);
   if (!deletedBootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
@@ -171,7 +182,7 @@ const getBootcampsInRadius = asyncHandler(async (req, res, next) => {
     radius = radiusKm;
   }
 
-  const bootcamps = await BootCamp.find({
+  const bootcamps = await Bootcamp.find({
     location: {
       $geoWithin: {
         $centerSphere: [[lng, lat], radius],
@@ -191,7 +202,7 @@ const getBootcampsInRadius = asyncHandler(async (req, res, next) => {
 // @route       PUT /api/v1/bootcamps/:id/photo
 // @access      private
 const bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
-  const bootcamp = await BootCamp.findById(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
@@ -228,7 +239,7 @@ const bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
       return next(new ErrorResponse(`Problem with file upload`, 500));
     }
 
-    const updateBootcamp = await BootCamp.findByIdAndUpdate(req.params.id, { photo: file.name });
+    const updateBootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
 
     res.status(200).json({
       success: true,
